01ab2011c11d460908725c9fbcd4880c192b2b0f
andrequant <48969772+andrequant@users.noreply.github.com>
2026-08-31 12:24:00 +0100

    Add: remove-element - submission-8

---
+++ Data Structures & Algorithms/remove-element/submission-8.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] == val:
+                nums[i] = 10000
+            else:
+                k += 1
+        
+        nums.sort()
+
+        return k
