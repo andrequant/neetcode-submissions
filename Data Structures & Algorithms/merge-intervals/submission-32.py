803f7a8d4a4ba074d9f87df532f6fe11dd31125f
andrequant <48969772+andrequant@users.noreply.github.com>
2026-09-07 18:28:30 +0100

    Add: merge-intervals - submission-32

---
+++ Data Structures & Algorithms/merge-intervals/submission-32.py
@@ -0,0 +1,33 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        
+        if len(intervals) == 1:
+            return intervals
+
+        intervals.sort(key=lambda x: x[0])
+
+        i = 0
+        
+        merged = [intervals[0]]
+
+        while i < len(intervals)-1:
+            if (intervals[i+1][0] <= merged[-1][1]):
+                merged[-1][1] = max(merged[-1][1], intervals[i+1][1])
+            else:
+                merged.append(intervals[i+1])
+            i += 1
+
+        return merged
+
+
+
+    def mergeTwo(self, inter1, inter2):
+        
+        inter_new = [0,0]
+
+        if (inter2[0] <= inter1[1]):
+            inter_new[0] = inter1[0]
+            inter_new[1] = max(inter1[1], inter2[1])
+            return inter_new
+        else:
+            return None
